7f7cd285bea9231249c3a200612700e1ee190d0c
Aland Mariwan <amariwan9@icloud.com>
2024-07-19 14:46:31 +0200

    Update pokemonAPI.ts

--- lib/pokemonAPI.ts
+++ lib/pokemonAPI.ts
@@ -1,6 +1,6 @@
 export const getPokemonList = async () => {
   const response = await fetch(
-    "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0"
+    "https://pokeapi.co/api/v2/pokemon?limit=1000&offset=0"
   );
   const data = await response.json();
   return data.results;
