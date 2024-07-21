0244aec6f374870a0bd4dc22e74cd9253140701c
Aland Mariwan <dev@tasiomind.de>
2024-07-21 16:30:21 +0200

    feat: Update StatsBar component to use green color

--- src/components/stats-bar.tsx
+++ src/components/stats-bar.tsx
@@ -1,29 +1,25 @@
-"use client";
-import React, { useEffect, useRef } from "react";
-import gsap from "gsap";
+'use client';
+import React, { useEffect, useRef } from 'react';
+import gsap from 'gsap';
 
 type Props = {
-  value: number;
+	value: number;
 };
 
 export const StatsBar = ({ value }: Props) => {
-  const barRef = useRef<HTMLDivElement>(null!);
+	const barRef = useRef<HTMLDivElement>(null!);
 
-  useEffect(() => {
-    gsap.to(barRef.current, {
-      width: `${(value / 255) * 100}%`,
-      duration: 0.7,
-      ease: `rough({ template: bounce.out, strength: 1, points: 10, taper: out, randomize: true, clamp: false})`,
-    });
-  }, [value]);
+	useEffect(() => {
+		gsap.to(barRef.current, {
+			width: `${(value / 255) * 100}%`,
+			duration: 0.7,
+			ease: `rough({ template: bounce.out, strength: 1, points: 10, taper: out, randomize: true, clamp: false})`,
+		});
+	}, [value]);
 
-  return (
-    <div className="bg-gray-200 w-full h-4 rounded-full overflow-hidden">
-      <div
-        ref={barRef}
-        className="bg-black h-full"
-        style={{ width: "0%" }}
-      ></div>
-    </div>
-  );
+	return (
+		<div className='bg-gray-200 w-full h-4 rounded-full overflow-hidden'>
+			<div ref={barRef} className='h-full bg-green-600' style={{ width: '0%' }}></div>
+		</div>
+	);
 };
