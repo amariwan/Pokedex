39f018eb32d1df2fa7d1bb1826053fe39645e692
Aland Mariwan <dev@tasiomind.de>
2024-07-23 00:12:31 +0200

    chore: Update npm dependencies and add devDependencies
    This commit updates the npm dependencies in the package.json file. It adds the "@typescript-eslint/eslint-plugin" and "@typescript-eslint/parser" packages as devDependencies. It also updates the "eslint" package to version 9.7.0 and the "eslint-plugin-react" package to version 7.35.0.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -17,6 +17,7 @@ const buttonVariants = cva('inline-flex items-center justify-center rounded-md t
 			default: 'h-10 py-2 px-4',
 			sm: 'h-9 px-3 rounded-md',
 			lg: 'h-11 px-8 rounded-md',
+			icon: '',
 		},
 	},
 	defaultVariants: {
